1677e05ea0ed97a7a0700e7e244f626a7cad565c
detextre4 <detextre4@gmail.com>
2022-04-20 09:51:35 -0400

    first commit

---
+++ src/components/Layout/Header.vue
@@ -0,0 +1,150 @@
+<template>
+  <section>
+    <!-- mobile -->
+
+    <!-- desktop -->
+    <v-app-bar
+    id="headerApp"
+    height="100px"
+    fixed
+  >
+    <v-row>
+      <v-col cols="12" sm="11" class="space align paddbottomdel">
+        <aside class="contleft divrow acenter">
+          <!-- toggle -->
+          <v-btn class="toggle vermobile" width="2.8em" height="2.8em" rounded
+            @click.stop="ShowDrawer()">
+            <v-icon size="clamp(1.5em, 2vw, 2em)" color="#FFFFFF !important">mdi-menu</v-icon>
+          </v-btn>
+          <!-- logo -->
+          <router-link to="/" class="eliminarmobile">
+            <img class="logo" src="@/assets/logos/logo.png"
+              alt="Logo">
+          </router-link>
+        </aside>
+
+        <v-col class="jend padd eliminarmobile">
+          <!-- content -->
+          <v-col class="contmiddle end padd">
+            <a :href="item.link" v-for="(item, index) in dataHeader" :key="index"
+              class="h9-em openMenuCollections notHover">
+              {{ item.title }}
+            </a>
+          </v-col>
+        </v-col>
+
+        <aside class="contright">
+          <v-btn class="walletButton" color="#F29627" rounded>
+            Connect Wallet
+          </v-btn>
+        </aside>
+      </v-col>
+    </v-row>
+  </v-app-bar>
+
+  <MenuHeader ref="menu"></MenuHeader>
+  </section>
+</template>
+
+<script>
+import MenuHeader from "./MenuHeader.vue"
+
+let scrollValue =
+document.body.scrollTop || document.documentElement.scrollTop;
+let resizeTimeout;
+function resizeThrottler(actualResizeHandler) {
+  // ignore resize events as long as an actualResizeHandler execution is in the queue
+  if (!resizeTimeout) {
+    resizeTimeout = setTimeout(() => {
+      resizeTimeout = null;
+      actualResizeHandler();
+
+      // The actualResizeHandler will execute at a rate of 15fps
+    }, 80);
+  }
+}
+export default {
+  name: "Header",
+  components: {
+    MenuHeader
+  },
+  i18n: require("./i18n"),
+  // created() {
+  //   this.element = document.getElementById("theme");
+  //   const theme = localStorage.getItem("theme");
+  //   if (theme) {
+  //     this.CambiarTheme(theme);
+  //   }
+  //   if (theme == "light") {
+  //     this.themeButton = true;
+  //   }
+  //   if (theme == "dark") {
+  //     this.themeButton = false;
+  //   }
+  // },
+  data() {
+    return {
+      // themeButton: true,
+      dataHeader: [
+        {
+          title: "Inicio",
+          link: "#"
+        },
+        {
+          title: "Cursos",
+          link: "#"
+        },
+        // {
+        //   title: "Mi Perfil",
+        //   link: "#"
+        // },
+        {
+          title: "Instructor",
+          link: "#"
+        },
+        {
+          title: "Mi Aprendizaje",
+          link: "#"
+        },
+        {
+          title: "Contact",
+          link: "#"
+        }
+      ]
+    };
+  },
+  methods: {
+    ShowDrawer() {
+      this.$refs.menu.ShowDrawer();
+    },
+    // CambiarTheme(theme) {
+    //   this.$store.dispatch("CambiarTheme", { theme, element: this.element });
+    //   this.themeButton = !this.themeButton;
+    // },
+    // CambiarTheme2(theme) {
+    //   this.$refs.menu.OverlayMethod(theme);
+    // },
+    OcultarHeader() {
+      // in top
+      let Desplazamiento_Actual = window.pageYOffset;
+      if (Desplazamiento_Actual > scrollValue) {
+        document.getElementById("headerApp").style.background = "#FFFFFF";
+      } else {
+        document.getElementById("headerApp").style.background = "transparent";
+      }
+    },
+    scrollListener() {
+      resizeThrottler(this.OcultarHeader);
+    }
+  },
+  mounted() {
+    document.getElementById("headerApp").style.background = "transparent";
+    document.addEventListener('scroll', this.scrollListener);
+  },
+  beforeDestroy() {
+    document.removeEventListener('scroll', this.scrollListener);
+  }
+};
+</script>
+
+<style src="./Layout.scss" lang="scss" />
---
+++ src/pages/Home/Home.vue
@@ -0,0 +1,327 @@
+<template>
+  <section id="home" class="parent">
+    <v-col id="hero" class="spacea divwrap">
+      <h1 class="divcol h1-em">
+        CON NEAR EL ESTUDIO E-LEARNING ES AHORA MAS FACIL
+        <h6>Disponibles 24/7</h6>
+      </h1>
+      <img src="@/assets/images/img-intro.png" alt="Image background">
+    </v-col>
+
+    <v-card color="#E5E5E5" class="contInfo">
+      <aside v-for="(item, index) in dataInfo" :key="index"
+        class="divrow acenter">
+        <img :src="item.icon" alt="Icon About Info">
+        <div class="divcol jcenter">
+          <h3 class="h4-em p">{{ item.title }}</h3>
+          <span class="h10-em">{{ item.desc }}</span>
+        </div>
+      </aside>
+    </v-card>
+
+    <v-col class="contDestacado">
+      <h2 class="h4-em tcenter">LO MAS DESTACADO</h2>
+      <aside class="contslider">
+        <v-carousel
+          v-model="carouselDestacado"
+          hide-delimiters
+          show-arrows-on-hover
+        >
+          <template v-for="(item, index) in sliderDestacado">
+            <v-carousel-item
+              v-if="(index + 1) % columnsDestacado === 1 || columnsDestacado === 1"
+              :key="index"
+            > 
+              <v-row class="fill-height spacea divpadd">
+                <template v-for="(n,i) in columnsDestacado">
+                  <template v-if="(+index + i) < sliderDestacado.length">
+                    <v-col :key="i">
+                      <v-card color="var(colorCartas)" class="cartas divcol">
+                        <img :src="sliderDestacado[+index + i].img" alt="Imagen curso">
+                        <div class="divcol astart">
+                          <a :href="sliderDestacado[+index + i].to" class="h7-em bold"
+                            style="color: #FF6B3B !important">
+                            {{sliderDestacado[+index + i].title }}
+                          </a>
+                          <p class="h8-em p">{{sliderDestacado[+index + i].desc}}</p>
+                          <v-rating
+                            v-model="sliderDestacado[+index + i].rating"
+                            background-color="pink lighten-3"
+                            color="orange"
+                            hover
+                          ></v-rating>
+                        </div>
+                      </v-card>
+                    </v-col>
+                  </template>
+                </template>
+              </v-row>
+            </v-carousel-item>
+          </template>
+        </v-carousel>
+      </aside>
+    </v-col>
+    
+    <v-col class="contDestacado">
+      <h2 class="h4-em tcenter">LO MAS RECIENTE</h2>
+      <aside class="contslider">
+        <v-carousel
+          v-model="carouselReciente"
+          hide-delimiters
+          show-arrows-on-hover
+        >
+          <template v-for="(item, index) in sliderReciente">
+            <v-carousel-item
+              v-if="(index + 1) % columnsReciente === 1 || columnsReciente === 1"
+              :key="index"
+            > 
+              <v-row class="fill-height spacea divpadd">
+                <template v-for="(n,i) in columnsReciente">
+                  <template v-if="(+index + i) < sliderReciente.length">
+                    <v-col :key="i">
+                      <v-card color="var(colorCartas)" class="cartas divcol">
+                        <img :src="sliderReciente[+index + i].img" alt="Imagen curso">
+                        <div class="divcol astart">
+                          <a :href="sliderReciente[+index + i].to" class="h7-em bold">
+                            {{sliderReciente[+index + i].title }}
+                          </a>
+                          <p class="h8-em p">{{sliderReciente[+index + i].desc}}</p>
+                        </div>
+                      </v-card>
+                    </v-col>
+                  </template>
+                </template>
+              </v-row>
+            </v-carousel-item>
+          </template>
+        </v-carousel>
+      </aside>
+    </v-col>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {
+      dataInfo: [
+        {
+          icon: require("@/assets/icons/idiomas.png"),
+          title: "+ de 500",
+          desc: "PROFESIONALES DE MÚLTIPLES IDIOMAS"
+        },
+        {
+          icon: require("@/assets/icons/acceso.png"),
+          title: "Acceso",
+          desc: "ILIMITADO"
+        },
+        {
+          icon: require("@/assets/icons/cursos.png"),
+          title: "100K",
+          desc: "CURSOS ONLINE"
+        }
+      ],
+      carouselDestacado: 0,
+      sliderDestacado: [
+        {
+          img: require("@/assets/images/cursos.png"),
+          title: "Lunaroid",
+          desc: "Lunaroid is the first high art-intensive NFT hitting the block...",
+          to: "#",
+          rating: ""
+        },
+        {
+          img: require("@/assets/images/cursos.png"),
+          title: "Lunaroid",
+          desc: "Lunaroid is the first high art-intensive NFT hitting the block...",
+          to: "#",
+          rating: ""
+        },
+        {
+          img: require("@/assets/images/cursos.png"),
+          title: "Lunaroid",
+          desc: "Lunaroid is the first high art-intensive NFT hitting the block...",
+          to: "#",
+          rating: ""
+        },
+        {
+          img: require("@/assets/images/cursos.png"),
+          title: "Lunaroid",
+          desc: "Lunaroid is the first high art-intensive NFT hitting the block...",
+          to: "#",
+          rating: ""
+        },
+        {
+          img: require("@/assets/images/cursos.png"),
+          title: "Lunaroid",
+          desc: "Lunaroid is the first high art-intensive NFT hitting the block...",
+          to: "#",
+          rating: ""
+        },
+        {
+          img: require("@/assets/images/cursos.png"),
+          title: "Lunaroid",
+          desc: "Lunaroid is the first high art-intensive NFT hitting the block...",
+          to: "#",
+          rating: ""
+        },
+        {
+          img: require("@/assets/images/cursos.png"),
+          title: "Lunaroid",
+          desc: "Lunaroid is the first high art-intensive NFT hitting the block...",
+          to: "#",
+          rating: ""
+        },
+        {
+          img: require("@/assets/images/cursos.png"),
+          title: "Lunaroid",
+          desc: "Lunaroid is the first high art-intensive NFT hitting the block...",
+          to: "#",
+          rating: ""
+        },
+        {
+          img: require("@/assets/images/cursos.png"),
+          title: "Lunaroid",
+          desc: "Lunaroid is the first high art-intensive NFT hitting the block...",
+          to: "#",
+          rating: ""
+        },
+        {
+          img: require("@/assets/images/cursos.png"),
+          title: "Lunaroid",
+          desc: "Lunaroid is the first high art-intensive NFT hitting the block...",
+          to: "#",
+          rating: ""
+        },
+      ],
+      carouselReciente: 0,
+      sliderReciente: [
+        {
+          img: require("@/assets/images/cursos.png"),
+          title: "Lunaroid",
+          desc: "Lunaroid is the first high art-intensive NFT hitting the block...",
+          to: "#",
+          rating: ""
+        },
+        {
+          img: require("@/assets/images/cursos.png"),
+          title: "Lunaroid",
+          desc: "Lunaroid is the first high art-intensive NFT hitting the block...",
+          to: "#",
+          rating: ""
+        },
+        {
+          img: require("@/assets/images/cursos.png"),
+          title: "Lunaroid",
+          desc: "Lunaroid is the first high art-intensive NFT hitting the block...",
+          to: "#",
+          rating: ""
+        },
+        {
+          img: require("@/assets/images/cursos.png"),
+          title: "Lunaroid",
+          desc: "Lunaroid is the first high art-intensive NFT hitting the block...",
+          to: "#",
+          rating: ""
+        },
+        {
+          img: require("@/assets/images/cursos.png"),
+          title: "Lunaroid",
+          desc: "Lunaroid is the first high art-intensive NFT hitting the block...",
+          to: "#",
+          rating: ""
+        },
+        {
+          img: require("@/assets/images/cursos.png"),
+          title: "Lunaroid",
+          desc: "Lunaroid is the first high art-intensive NFT hitting the block...",
+          to: "#",
+          rating: ""
+        },
+        {
+          img: require("@/assets/images/cursos.png"),
+          title: "Lunaroid",
+          desc: "Lunaroid is the first high art-intensive NFT hitting the block...",
+          to: "#",
+          rating: ""
+        },
+        {
+          img: require("@/assets/images/cursos.png"),
+          title: "Lunaroid",
+          desc: "Lunaroid is the first high art-intensive NFT hitting the block...",
+          to: "#",
+          rating: ""
+        },
+        {
+          img: require("@/assets/images/cursos.png"),
+          title: "Lunaroid",
+          desc: "Lunaroid is the first high art-intensive NFT hitting the block...",
+          to: "#",
+          rating: ""
+        },
+        {
+          img: require("@/assets/images/cursos.png"),
+          title: "Lunaroid",
+          desc: "Lunaroid is the first high art-intensive NFT hitting the block...",
+          to: "#",
+          rating: ""
+        },
+      ],
+    }
+  },
+  computed: {
+    columnsDestacado() {
+      if (this.$vuetify.breakpoint.xl) {
+        return 5;
+      }
+
+      // if (this.$vuetify.breakpoint.lg && window.innerWidth < 1306) {
+      //   return 3;
+      // }
+      if (this.$vuetify.breakpoint.lg) {
+        return 5;
+      }
+
+      if (this.$vuetify.breakpoint.md) {
+        return 4;
+      }
+
+      if (this.$vuetify.breakpoint.sm) {
+        return 2;
+      }
+
+      return 1;
+    },
+    columnsReciente() {
+      if (this.$vuetify.breakpoint.xl) {
+        return 5;
+      }
+
+      // if (this.$vuetify.breakpoint.lg && window.innerWidth < 1306) {
+      //   return 3;
+      // }
+      if (this.$vuetify.breakpoint.lg) {
+        return 5;
+      }
+
+      if (this.$vuetify.breakpoint.md) {
+        return 4;
+      }
+
+      if (this.$vuetify.breakpoint.sm) {
+        return 2;
+      }
+
+      return 1;
+    },
+  },
+  methods: {
+    SelectCardDestacado(item) {
+      console.log(item)
+    },
+  }
+};
+</script>
+
+<style src="./Home.scss" lang="scss" />
